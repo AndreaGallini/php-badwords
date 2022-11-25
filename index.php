b60d7a7b40fae3ef9fdbc26e6e9b429f36e89fc6
AndreaGallini <andrea.gallini01@libero.it>
2022-11-25 14:05:51 +0100

    add bootstrap and some grafic

--- index.php
+++ index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./styleIndex.css">
     <title>Document</title>
 </head>
 <body>
     <form action="./result.php" method="post">
-        <label for="fname">Inserisci il testo da censurare</label>
-        <input type="text" name="testo">
-        <label for="lname">Inserisci la parola da censurare</label>
+        <h1>Censuratore Boolean</h1>
+        <label for="text">Inserisci il testo da censurare</label>
+        <div class="input-group">
+  <textarea class="form-control widthtext" aria-label="With textarea" name="testo"></textarea>
+</div>
+        <label for="word">Inserisci la parola da censurare</label>
         <input type="text" name="parolaCensurata">
     <button type="submit">Invia</button>
 
--- result.php
+++ result.php
@@ -1,8 +1,7 @@
 <?php
 $testo = $_POST['testo'];
 $parolaCensurata = $_POST['parolaCensurata'];
-echo $testo;
-echo $parolaCensurata;
+
 $lunghezzatesto = strlen($testo);
 $testoCensurato = str_replace($parolaCensurata, "***", $testo);
 ?>
@@ -12,14 +11,15 @@ $testoCensurato = str_replace($parolaCensurata, "***", $testo);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styleResult.css">
     <title>Document</title>
 </head>
 <body>
     <h2>Il tuo testo :</h2>
-    <p><?php echo $testo?></p>
+    <p class="text"><?php echo $testo?></p>
     <h2>Lunghezza del testo <?php echo $lunghezzatesto ?> caratteri</h2>
     <h2>Parola censurata : <?php echo $parolaCensurata?></h2>
     <h2>Testo censurato:</h2>
-    <p><?php echo $testoCensurato?></p>
+    <p class="text"><?php echo $testoCensurato?></p>
 </body>
 </html>
